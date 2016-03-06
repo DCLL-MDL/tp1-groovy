79b0543809ef10d16581f3d82bb2b5c4f099612b
raclet <raclet@irit.fr>
2016-03-06 06:19:49 +0100

    Update Exo6.groovy
    update URL

--- src/org/tp1/exo6/Exo6.groovy
+++ src/org/tp1/exo6/Exo6.groovy
@@ -10,7 +10,7 @@ package org.tp1.exo6
 class Exo6 extends GroovyTestCase {
 
     void test01_WithMethod() {
-        // The 'with()' method [ http://groovy.codehaus.org/groovy-jdk/java/lang/Object.html#with(groovy.lang.Closure) ]
+        // The 'with()' method [ http://docs.groovy-lang.org/latest/html/groovy-jdk/java/lang/Object.html#with(groovy.lang.Closure) ]
         // allows you to access an object within a closure without explicitly referring to it.
 
         // This is how Java StringBuilders are used:
@@ -32,9 +32,9 @@ class Exo6 extends GroovyTestCase {
     void test02_CollectMethodOnLists() {
         // We are often required to iterate through a whole list and and perform some sort of transformation on
         // some (or all) of the items, returning a new list. Groovy has a method just for that:
-        // http://groovy.codehaus.org/groovy-jdk/java/lang/Object.html#collect(groovy.lang.Closure)
+        // http://docs.groovy-lang.org/latest/html/groovy-jdk/java/lang/Object.html#collect(groovy.lang.Closure)
 
-        // Using collect() and a method from http://groovy.codehaus.org/groovy-jdk/java/util/Collection.html, create
+        // Using collect() and a method from http://docs.groovy-lang.org/latest/html/groovy-jdk/java/util/Collection.html, create
         // a list of UNIQUE class types from the following list:
         def differentTypes = [1, 'String', "GString", 'a', 'Another string', 0]
         def uniqueTypes = []
@@ -46,7 +46,7 @@ class Exo6 extends GroovyTestCase {
 
     void test03_FileIteration() {
         // Groovy's File enhancements includes an iterator that walks through all files
-        // http://groovy.codehaus.org/groovy-jdk/java/io/File.html#eachFileRecurse(groovy.lang.Closure)
+        // http://docs.groovy-lang.org/latest/html/groovy-jdk/java/io/File.html#eachFileRecurse(groovy.lang.Closure)
 
         // Use the eachFileRecurse iterator to find the number of files that contain the string 'Lorem'
         // under the src directory
@@ -59,7 +59,7 @@ class Exo6 extends GroovyTestCase {
     }
 
     void test04_ConcludingExercise() {
-        // Using methods from Groovy object (http://groovy.codehaus.org/groovy-jdk/java/lang/Object.html) and
+        // Using methods from Groovy object http://docs.groovy-lang.org/latest/html/groovy-jdk/java/lang/Object.html and
         // range objects, store all the prime numbers between 200 and 250 in the target variable
         def primesBetween200And250 = []
         // ------------ START EDITING HERE ----------------------
